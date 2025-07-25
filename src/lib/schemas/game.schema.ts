f444138b367d11289e9dbccc4e51f2005ce77c32
dannyk-dev <kruger.dkk@gmail.com>
2025-07-25 09:42:49 -0300

    fix

--- src/lib/schemas/game.schema.ts
+++ src/lib/schemas/game.schema.ts
@@ -1,11 +1,10 @@
-import { roomQuerySchema } from "@/lib/schemas";
 import z from "zod";
 
 export const gameQuerySchema = z.object({
   gameId: z.string()
 });
 
-export const endGameSchema = gameQuerySchema.merge(roomQuerySchema);
-export const voteSchema = endGameSchema.extend({
-  value: z.number().int().positive()
-});
+// export const endGameSchema = gameQuerySchema.merge(roomQuerySchema);
+// export const voteSchema = endGameSchema.extend({
+//   value: z.number().int().positive()
+// });
--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -1,9 +1,18 @@
 import z from 'zod';
 
-export * from './game.schema';
+
 export * from './players.schema';
 export * from './room.schema'
 
 export const roomQuerySchema = z.object({
   roomId: z.string()
+});
+
+export const gameQuerySchema = z.object({
+  gameId: z.string()
+});
+
+export const endGameSchema = gameQuerySchema.merge(roomQuerySchema);
+export const voteSchema = endGameSchema.extend({
+  value: z.number().int().positive()
 });
