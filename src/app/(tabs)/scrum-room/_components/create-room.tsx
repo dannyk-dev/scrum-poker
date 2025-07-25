170c8dab54ba7bb78932b901d287ef132c610e1b
dannyk-dev <kruger.dkk@gmail.com>
2025-07-25 15:01:12 -0300

    fix

--- src/app/(tabs)/scrum-room/_components/create-room.tsx
+++ src/app/(tabs)/scrum-room/_components/create-room.tsx
@@ -3,8 +3,10 @@
 import ScrumRoomForm from "@/app/(tabs)/scrum-room/_components/scrum-room-form";
 import PopoverForm from "@/components/ui/popover-form";
 import type { TRoomSchema } from "@/lib/schemas/room.schema";
+import { cn } from "@/lib/utils";
 import { api } from "@/trpc/react";
 import { IconCirclePlus } from "@tabler/icons-react";
+import { useMediaQuery } from "@uidotdev/usehooks";
 import { useRouter } from "next/navigation";
 import React, { useCallback, useState } from "react";
 import { toast } from "sonner";
@@ -12,6 +14,7 @@ import { toast } from "sonner";
 const CreateRoom = () => {
   const [open, setOpen] = useState(false);
   const { mutate, isPending } = api.room.createRoom.useMutation();
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
   const router = useRouter();
 
   const handleSubmit = useCallback(
@@ -42,7 +45,7 @@ const CreateRoom = () => {
       setOpen={setOpen}
       width="350px"
       isAction
-      popupClass="top-8 right-5"
+      popupClass={cn("top-8", isSmallDevice && 'right-5')}
       height="200px"
       showCloseButton={true}
       openChild={
