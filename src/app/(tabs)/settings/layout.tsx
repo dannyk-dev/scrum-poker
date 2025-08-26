fcab7cba067a193f946232371af5fcc94b09127d
dannyk-dev <kruger.dkk@gmail.com>
2025-08-26 16:45:36 -0300

    update

--- src/app/(tabs)/settings/layout.tsx
+++ src/app/(tabs)/settings/layout.tsx
@@ -7,8 +7,8 @@ import React, { type PropsWithChildren } from 'react'
 
 const ProfileLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-full md:w-[90%] md:mx-auto min-h-screen flex items-center justify-center">
-      <Card className='relative z-10 w-full max-w-screen gap-y-6 shadow-2xl md:h-[80vh] 2xl:h-[85vh] md:max-h-[80vh]'>
+    <div className="w-full md:w-[90%] md:mx-auto min-h-screen flex items-center justify-center pb-14">
+      <Card className='relative z-10 w-full max-w-screen gap-y-6 shadow-2xl md:h-[85vh] 2xl:h-[85vh] md:max-h-[85vh]'>
       <CardHeader>
         <CardTitle className='text-xl font-semibold px-2'>My Profile</CardTitle>
         <CardDescription className='px-2'>
